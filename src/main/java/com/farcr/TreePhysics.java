bd527c092a1bb8bea3316cc02eb2321a8e161af5
KyanBirb <97422254+KyanBirb@users.noreply.github.com>
2026-04-22 22:58:42 -0500

    Basic tree detection prototyping

--- src/main/java/com/farcr/TreePhysics.java
+++ src/main/java/com/farcr/TreePhysics.java
@@ -1,9 +1,11 @@
 package com.farcr;
 
+import com.farcr.event.CommonEvents;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(TreePhysics.MOD_ID)
@@ -12,7 +14,7 @@ public class TreePhysics {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TreePhysics(IEventBus modEventBus, ModContainer modContainer) {
-
+        NeoForge.EVENT_BUS.register(CommonEvents.class);
     }
 
 }
