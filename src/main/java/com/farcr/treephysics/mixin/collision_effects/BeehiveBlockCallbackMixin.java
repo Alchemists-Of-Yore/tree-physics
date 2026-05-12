38417cb1921c7da0f14d88321ab828da801f6073
KyanBirb <97422254+KyanBirb@users.noreply.github.com>
2026-05-12 02:27:12 -0500

    Make everything in falls_from_trees fragile

--- src/main/java/com/farcr/treephysics/mixin/collision_effects/BeehiveBlockCallbackMixin.java
+++ src/main/java/com/farcr/treephysics/mixin/collision_effects/BeehiveBlockCallbackMixin.java
@@ -1,5 +1,6 @@
 package com.farcr.treephysics.mixin.collision_effects;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -13,6 +14,11 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BeehiveBlockCallback.class)
 public class BeehiveBlockCallbackMixin {
+
+    @WrapMethod(method = "getTriggerVelocity")
+    private double treephysics$getTriggerVelocity(Operation<Double> original) {
+        return 4.0;
+    }
 
     @WrapOperation(method = "onHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BeehiveBlockEntity;emptyAllLivingFromHive(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/entity/BeehiveBlockEntity$BeeReleaseStatus;)V"))
     private void treephysics$onHit(BeehiveBlockEntity instance, Player player, BlockState state, BeehiveBlockEntity.BeeReleaseStatus releaseStatus, Operation<Void> original, @Local(argsOnly = true) ServerLevel level) {
