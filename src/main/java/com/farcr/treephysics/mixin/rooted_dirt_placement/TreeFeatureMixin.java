35a931f9bb0109b999654472082c941609c2226b
KyanBirb <97422254+KyanBirb@users.noreply.github.com>
2026-05-17 13:00:57 -0500

    Make rooted dirt placement more invasive

---
+++ src/main/java/com/farcr/treephysics/mixin/rooted_dirt_placement/TreeFeatureMixin.java
@@ -0,0 +1,38 @@
+package com.farcr.treephysics.mixin.rooted_dirt_placement;
+
+import com.farcr.treephysics.index.TreePhysicsTags;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.TreeFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.function.BiConsumer;
+
+@Mixin(TreeFeature.class)
+public class TreeFeatureMixin {
+
+    @WrapOperation(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/feature/TreeFeature;doPlace(Lnet/minecraft/world/level/WorldGenLevel;Lnet/minecraft/util/RandomSource;Lnet/minecraft/core/BlockPos;Ljava/util/function/BiConsumer;Ljava/util/function/BiConsumer;Lnet/minecraft/world/level/levelgen/feature/foliageplacers/FoliagePlacer$FoliageSetter;Lnet/minecraft/world/level/levelgen/feature/configurations/TreeConfiguration;)Z"))
+    private boolean treephysics$doPlace(TreeFeature instance, WorldGenLevel level, RandomSource random, BlockPos pos, BiConsumer<BlockPos, BlockState> rootBlockSetter, BiConsumer<BlockPos, BlockState> trunkBlockSetter, FoliagePlacer.FoliageSetter foliageBlockSetter, TreeConfiguration config, Operation<Boolean> original) {
+        BiConsumer<BlockPos, BlockState> pawesomeTrunkBlockSetter = (blockPos, state) -> {
+            trunkBlockSetter.accept(blockPos, state);
+            if(state.is(BlockTags.LOGS) && state.hasProperty(RotatedPillarBlock.AXIS) && state.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y) {
+                if (level.getBlockState(blockPos.below()).is(TreePhysicsTags.CAN_BE_ROOTS)) {
+                    level.setBlock(blockPos.below(), Blocks.ROOTED_DIRT.defaultBlockState(), 19);
+                }
+            }
+        };
+        return original.call(instance, level, random, pos, rootBlockSetter, pawesomeTrunkBlockSetter, foliageBlockSetter, config);
+    }
+
+}
